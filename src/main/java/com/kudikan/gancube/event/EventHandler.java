771d20d5106d521844aafb85c9fa47e15198c17d
ホ篴坎 <65447752+KuDikan@users.noreply.github.com>
2023-08-17 13:40:16 +0800

    Update EventHandler.java

--- src/main/java/com/kudikan/gancube/event/EventHandler.java
+++ src/main/java/com/kudikan/gancube/event/EventHandler.java
@@ -14,6 +14,7 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 import net.minecraft.item.crafting.CraftingManager;
 
 @Mod.EventBusSubscriber
