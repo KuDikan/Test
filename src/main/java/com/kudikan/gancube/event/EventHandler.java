4f8d0235cd0d32214efc8bbfafb22312b20448bf
ホ篴坎 <65447752+KuDikan@users.noreply.github.com>
2023-08-17 10:45:07 +0800

    Update EventHandler.java

--- src/main/java/com/kudikan/gancube/event/EventHandler.java
+++ src/main/java/com/kudikan/gancube/event/EventHandler.java
@@ -1,8 +1,12 @@
 package com.kudikan.gancube.event;
 
+import com.kudikan.gancube.event.DummyRecipe;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
@@ -10,4 +14,15 @@ public class EventHandler {
     public static void onPlayerJoin(EntityJoinWorldEvent event){
 
     }
-}
+    @SubscribeEvent
+	public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+		IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) event.getRegistry();
+		RemoveRecipe(modRegistry, new ResourceLocation("minecraft:bed"));
+    }
+    public void removeRecipes(IForgeRegistryModifiable modRegistry, ResourceLocation recipe) {
+        IRecipe p = (IRecipe)modRegistry.getValue(recipe);
+		
+		modRegistry.remove(recipe);
+		modRegistry.register(DummyRecipe.from(p));
+    }
+}   
